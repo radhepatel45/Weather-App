cb8058b024848f0d676adf58b2fa5c2e56cdce6f
Radhe Patel <163132006+radhepatel45@users.noreply.github.com>
2025-01-04 15:45:05 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// API configuration
+const CONFIG = {
+    API_KEY: '3535586856795c79bc3b4924d11802c8', 
+    BASE_URL: 'https://api.openweathermap.org/data/2.5/weather'
+};
+
+// https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}
